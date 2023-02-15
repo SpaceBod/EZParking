cece6eaf6cd69195aa900957495b1fb116fae83b
Luca <lucabod8@gmail.com>
2023-02-15 14:48:10 +0000

    Revert "Update data.json"

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -7,17 +7,7 @@ export const Services = (props) => {
         <div className="section-title">
           <h2>Technologies</h2>
           <p>
-<<<<<<< HEAD
-<<<<<<< HEAD
             Take a closer look inside...
-=======
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
->>>>>>> parent of a129e06 (updated)
-=======
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
->>>>>>> parent of a129e06 (updated)
           </p>
         </div>
         <div className="row">
