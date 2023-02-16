d92a306d915709bc2c9f1469693e44920ad76de2
Luca <lucabod8@gmail.com>
2023-02-16 10:45:56 +0000

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -13,7 +13,7 @@ export const Header = (props) => {
                   <span></span>
                 </h1>
                 <p>{props.data ? props.data.paragraph : "Loading"}</p>
-                <a href="146.169.150.120:5000" className="btn btn-custom btn-lg page-scroll">
+                <a href="http://146.169.150.120:5000" className="btn btn-custom btn-lg page-scroll">
                   Parking Portal
                 </a>
               </div>
